d21ef3976fa8ea5b5b9bc343027deec276fdc126
kr1506 <154000192+kr1506@users.noreply.github.com>
2024-06-03 00:39:10 +0530

    Add files via upload

--- program1.py
+++ program1.py
@@ -1,6 +1,9 @@
 def encipher(message, r):
     a = 'a'  
-    z = 'z'  
+    z = 'z'
+    message.lower()
+    message.strip()
+    message.replace(" ","")
     list_message = list(message)
     for i in range(len(message)):
         ch = list_message[i]
@@ -13,12 +16,15 @@ def encipher(message, r):
 def decipher(emessage,r):
     a = 'a'
     z = 'z'
+    emessage.lower()
+    emessage.strip()
+    emessage.replace(" ","")
     list_emessage=list(emessage)
     for i in range(len(emessage)):
         ch=list_emessage[i]
         if ch>='a' and ch<='z' :
             ch=chr(ord(ch)-r)
-            if ch<='a' :
+            if ch<'a' :
                 ch=chr(ord(ch)+ord(z)-ord(a)+1)
             list_emessage[i]=ch
     return ''.join(list_emessage)
@@ -26,13 +32,13 @@ def decipher(emessage,r):
 user_input=input("what do you want to do: Encryption=type e,Decryption=type d")
 if user_input=='e':
     message=input("enter message :")
-    r = 3
+    r = int(input("key:"))
     enciphered_message = encipher(message, r)
     print(enciphered_message)
     print(len(enciphered_message))
 if user_input=='d':
     emessage=input("enter emessage :")
-    r = 3
+    r = int(input("key:"))
     deciphered_message = decipher(emessage, r)
     print(deciphered_message)
     print(len(deciphered_message))
